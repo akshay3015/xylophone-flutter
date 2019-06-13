faa3f4fd1fdfa6121a419a64758e2334aaa2864a
akshayshahane <akshay.shahane13@gmail.com>
2019-06-14 00:26:38 +0530

    feat : learnt to play soun

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  final colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.teal,
+    Colors.green,
+    Colors.white,
+    Colors.amber,
+    Colors.purple
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: FlatButton(
+              onPressed: () {},
+              child: Text("Let's play something"),
+            ),
+          ),
         ),
       ),
     );
